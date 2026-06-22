9bd6ab8e8741eab06b2f497bf1fdb30d962c60e6
pavithra1711 <1711.pavithra@gmail.com>
2026-06-22 13:14:57 +0530

    fix: improve mobile responsiveness for homepage UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,37 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-
-  title = 'physio-frontend';
-
-  menuOpen = false;
-
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen;
-  }
-
-  closeMenu() {
-    this.menuOpen = false;
-  }
-
-  <nav class="navbar">
-
-  <img src="assets/logo.svg" class="logo">
-
-  <!-- Hamburger -->
-  <div class="menu-icon" (click)="toggleMenu()">
-    ☰
-  </div>
-
-  <!-- Menu -->
-  <ul class="nav-links" [class.active]="menuOpen">
-    <li><a (click)="closeMenu()" href="#home">Home</a></li>
-    <li><a (click)="closeMenu()" href="#services">Services</a></li>
-    <li><a (click)="closeMenu()" href="#contact">Contact Us</a></li>
-    <li><a (click)="closeMenu()" href="#about">About Us</a></li>
-  </ul>
-
-</nav>
+  
 }
+
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  menuOpen = false;
+
+toggleMenu() {
+  this.menuOpen = !this.menuOpen;
+}
+
+closeMenu() {
+  this.menuOpen = false;
+}
 
 }
